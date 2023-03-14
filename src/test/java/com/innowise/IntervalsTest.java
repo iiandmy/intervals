72e495373448827909e6b31908ff3c4bef3587e3
iiandmy <frezhgrand@gmail.com>
2023-03-14 15:16:57 +0300

    add test dependencies + test template

---
+++ src/test/java/com/innowise/IntervalsTest.java
@@ -0,0 +1,35 @@
+package com.innowise;
+
+import org.junit.jupiter.api.*;
+
+public class IntervalsTest {
+    @BeforeAll
+    static void setUpTests() {
+
+    }
+
+    @BeforeEach
+    void setupTestCase() {
+
+    }
+
+    @Test
+    void intervalIdentificationTest() {
+
+    }
+
+    @Test
+    void intervalConstructionTest() {
+
+    }
+
+    @AfterEach
+    void tearTestCase() {
+
+    }
+
+    @AfterAll
+    static void tear() {
+
+    }
+}
